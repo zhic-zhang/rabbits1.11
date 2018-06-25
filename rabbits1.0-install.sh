e822791f11f362537175586a490e8c54ad46a934
zhichengzhang2017 <zhangzhicheng2017@icloud.com>
2018-06-25 19:14:13 +0800

    Update rabbits1.0-install.sh

--- rabbits1.0-install.sh
+++ rabbits1.0-install.sh
@@ -1,36 +1,23 @@
 #!/bin/bash
-
 #download files
-wget https://raw.githubusercontent.com/zhichengzhang2017/rabbits1.11/master/rabbits1.0.zip && unzip rabbits1.0.zip
-
-#get IP
-read -p "please input your IP:" host_ip
-
+wget https://raw.githubusercontent.com/zhichengzhang2017/rabbits1.11/master/rabbits1.0.zip && unzip rabbits1.0.zip && read -p "please input your IP:" host_ip
 #set IP
 sed -i "s/123.206.81.19/$host_ip/g" `grep 123.206.81.19 -rl rabbits1.0/templates/`
 sed -i "s/123.206.81.19/$host_ip/g" rabbits1.0/flask_web.py
-
 #install pip
 python rabbits1.0/get-pip.py
-
 #install virtualenv
 pip install virtualenv
-
 #install yum
 yum -y install python-devel mariadb-server mariadb mysql-devel httpd
 yum groupinstall 'Development Tools'
 yum install -y zlib-dev openssl-devel sqlite-devel bzip2-devel mysql-community-devel
-
-
 #install flask
 pip install flask
-
 #install psutil
 pip install psutil
-
 #install MySQLdb
 pip install mysql-python
-
 #configure mariadb
 #if you meet "ERROR 1045 (28000)" please input "oracle" for passwd
 systemctl start mariadb
@@ -46,8 +33,6 @@ mysql -uroot  -poracle <<EOF
 		`time` int(11) DEFAULT NULL
         )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 EOF
-
-
 #start running scripts
 python monitor.py &
 python flask_web.py
